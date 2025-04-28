7a801b18305827e6d0937f83eb96e2a76e60464c
gagiyin <76633954+gagiyin@users.noreply.github.com>
2025-04-28 20:23:41 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
-<?php include "partials/header.php" ?>
-<?php include "partials/navbar.php" ?>
-
 <?php
 
+// Redirect function (i usually put it in the header or into the init file if you have partials created)
+function redirect($path) {
+    header("Location: ". $path);
+    exit();
+}
+
+// Processing the login
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -15,25 +19,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 ?>
 
-<div class="center">
-    <form class="user" method="POST">
+<div>
+    <form method="POST">
         <h1>Bejelentkezés</h1>
         <?php if (isset($_SESSION['notice'])): ?>
-        <div class="notice">
+        <div>
             <h3><?php echo $_SESSION['notice'] ?></h3>
         </div>
         <?php endif; ?>
-        <label for="email">E-mail cím:</label>
+        <label for="email">Email:</label>
         <input type="email" name="email" placeholder="| ">
-        <label for="password">Jelszó:</label>
+        <label for="password">Password:</label>
         <input type="password" name="password" placeholder="| ">
-        <div class="center" style="display: flex; flex-direction: column;">
-            <input class="button default" type="submit" value="Bejelentkezés" style="width: 50%">
-            <a href="register.php">Nincs még fiókod?</a>
+        <div class="center">
+            <input type="submit" value="Bejelentkezés">
+            <a href="register.php">Register</a>
         </div>
     </form>
 </div>
-
-<?php
-include "partials/footer.php";
-?>
